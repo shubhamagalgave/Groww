a2c553a68304dc982aebd53fbc0942017c06eaf9
shubham <shubhamagalgave11111@gmail.com>
2022-10-12 12:23:56 +0530

    file

---
+++ Built.java
@@ -0,0 +1,22 @@
+class Built
+{
+public static void main(String[] args)
+{
+String s ="RoomanTechnology";
+
+
+System.out.println(s.toUpperCase());
+System.out.println(s.toLowerCase());
+
+System.out.println(s.startsWith("R"));
+System.out.println(s.endsWith("s"));
+
+System.out.println(s.indexOf("T"));
+System.out.println(s.charAt(7));
+
+System.out.println(s.contains("Techno"));
+System.out.println(s.substring(6,12));
+System.out.println(s.substring(6));
+
+}
+}//Built in method
